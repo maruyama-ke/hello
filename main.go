6dccef2e8fa30cd3a22d8c68cf641e3b726e8680
unknown <>
2016-09-26 09:27:29 +0900

    [go]add net get

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import(
+  "net/http"
+  "log"
+  "io/ioutil"
+//  "encoding/json"
+//  "fmt"
+//  "reflect"
+)
+
+type pr_label struct {
+  Name string              `json:"name"`
+  Number int
+}
+
 func main() {
  println("hello!")
+ url := "https://api.github.com/repos/maruyama-ke/hello/issues"
+ resp, err := http.Get(url)
+ if err != nil {
+   log.Fatal(err)
+ }
+ defer resp.Body.Close()
+ body, err := ioutil.ReadAll(resp.Body)
+ if err != nil {
+   log.Fatal(err)
+ }
+ var issue_list = string(body)
+ println(issue_list)
 }
